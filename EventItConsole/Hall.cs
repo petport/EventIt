b7035ff1ada218643343c3d99b7fb71c349425a2
Petros Portokalakis <pportokalakis@tuc.gr>
2024-02-02 13:16:46 +0100

    Rename project

---
+++ EventItConsole/Hall.cs
@@ -0,0 +1,6 @@
+class Hall
+{
+    public required string Title { get; set; }
+    public required string Address { get; set; }
+    public required string Phone { get; set; }
+}
---
+++ EventItConsole/Theater.cs
@@ -0,0 +1,11 @@
+    class Theater
+    {
+        public required string Title { get; set; }
+        public required string By_who { get; set; }
+        public required string Type { get; set; }
+        public required string Duration { get; set; }
+        public required string Director { get; set; }
+        public required string Description { get; set; }
+        public required string Venue { get; set; }
+        public required string MoreInfoUrl { get; set; }
+    }
